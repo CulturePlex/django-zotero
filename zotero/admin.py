2539df7890e9c299d314fddd628fa25db0ed83dc
Antonio Jimenez Mavillard <ajimene6@uwo.ca>
2013-01-20 17:13:38 -0500

    Admin interface created.

--- zotero/admin.py
+++ zotero/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from zotero.models import ItemType
+from zotero.models import ItemType, Field, CreatorType
 
 admin.site.register(ItemType)
+admin.site.register(Field)
+admin.site.register(CreatorType)
