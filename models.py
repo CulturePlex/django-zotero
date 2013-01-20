1997ae42f5c6fd0baca4c8ff93c30c05c457ee22
Antonio Jimenez Mavillard <ajimene6@uwo.ca>
2013-01-20 16:05:54 -0500

    Initial structure created.

--- models.py
+++ models.py
@@ -3,16 +3,24 @@ from django.db import models
 # Create your models here.
 
 #Type of document: book, journal article, etc.
-class Item(models.Model):
+class ItemType(models.Model):
     type_name = models.CharField(max_length=100)
     
     def __unicode__(self):
         return self.type_name
 
-#Field in a document: title, author, etc.
+#Field in a document: title, publisher, publicationTitle, etc.
 class Field(models.Model):
     field_name = models.CharField(max_length=100)
-    item = models.ManyToManyField(Item)
+    item = models.ManyToManyField(ItemType)
     
     def __unicode__(self):
         return self.field_name
+
+#Type of creator: author, editor, etc.
+class CreatorType(models.Model):
+    type_name = models.CharField(max_length=100)
+    item = models.ManyToManyField(ItemType)
+    
+    def __unicode__(self):
+        return self.type_name
