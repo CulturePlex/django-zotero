6a36e8739a3d32251841c46e10b2ad1982fbb618
Antonio Jimenez Mavillard <ajimene6@uwo.ca>
2013-01-22 10:29:59 -0500

    Administration and TaggedItem model.

--- zotero/admin.py
+++ zotero/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
 from zotero.models import ItemType, Field, CreatorType
+from zotero.models import TaggedItem, FieldValue, CreatorValue
 
+#Metadata
+#class FieldInline(admin.TabularInline):
+#    model = Field
+#    extra = 0
+
+#class CreatorTypeInline(admin.TabularInline):
+#    model = CreatorType
+#    extra = 0
+
+#class ItemTypeAdmin(admin.ModelAdmin):
+#    inlines = [FieldInline,CreatorTypeInline]
+
+#admin.site.register(ItemType,ItemTypeAdmin)
 admin.site.register(ItemType)
 admin.site.register(Field)
 admin.site.register(CreatorType)
+
+#Data
+class FieldValueInline(admin.TabularInline):
+    model = FieldValue
+    extra = 0
+
+class CreatorValueInline(admin.TabularInline):
+    model = CreatorValue
+    extra = 0
+
+class TaggedItemAdmin(admin.ModelAdmin):
+    inlines = [FieldValueInline,CreatorValueInline]
+
+admin.site.register(TaggedItem,TaggedItemAdmin)
--- zotero/models.py
+++ zotero/models.py
@@ -1,18 +1,12 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
 
-# Create your models here.
 
-#Type of document: book, journal article, etc.
-class ItemType(models.Model):
-    type_name = models.CharField(max_length=100)
-    
-    def __unicode__(self):
-        return self.type_name
-
+# METADATA
 #Field in a document: title, publisher, publicationTitle, etc.
 class Field(models.Model):
     field_name = models.CharField(max_length=100)
-    item = models.ManyToManyField(ItemType)
     
     def __unicode__(self):
         return self.field_name
@@ -20,7 +14,47 @@ class Field(models.Model):
 #Type of creator: author, editor, etc.
 class CreatorType(models.Model):
     type_name = models.CharField(max_length=100)
-    item = models.ManyToManyField(ItemType)
     
     def __unicode__(self):
         return self.type_name
+
+#Type of document: book, journal article, etc.
+class ItemType(models.Model):
+    type_name = models.CharField(max_length=100)
+    fields = models.ManyToManyField(Field)
+    creators_types = models.ManyToManyField(CreatorType)
+    
+    def __unicode__(self):
+        return self.type_name
+
+
+#DATA
+#An instance of a tagged item: a book titled A, an article written by B, etc.
+class TaggedItem(models.Model):
+    item_type = models.ForeignKey(ItemType)
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey('content_type','object_id')
+    
+    def __unicode__(self):
+        fields_values = self.fieldvalue_set.all()
+        creators_values = self.creatorvalue_set.all()
+        return u'%s # %s # %s: (%s), (%s)' % (self.content_object,self.item_type,self.content_type,fields_values,creators_values)
+
+#A value for a field of a tagged item: "title='Yerma'"
+class FieldValue(models.Model):
+    field = models.ForeignKey(Field)
+    value = models.CharField(max_length=256)
+    tagged_item = models.ForeignKey(TaggedItem)
+    
+    def __unicode__(self):
+        return u'%s=%s' % (self.field,self.value)
+
+#A name for a creator of a tagged item: "author='Lorca'"
+class CreatorValue(models.Model):
+    creator_type = models.ForeignKey(CreatorType)
+    value = models.CharField(max_length=256)
+    tagged_item = models.ForeignKey(TaggedItem)
+    
+    def __unicode__(self):
+        return u'%s=%s' % (self.creator_type,self.value)
