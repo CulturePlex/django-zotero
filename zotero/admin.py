24e94d49e47506c5a53b140896e68dff3807f56a
Antonio Jimenez Mavillard <ajimene6@uwo.ca>
2013-01-29 10:26:07 -0500

    Enhanced adminstration and comments as docstrings.

--- zotero/admin.py
+++ zotero/admin.py
@@ -2,33 +2,78 @@ from django.contrib import admin
 from zotero.models import ItemType, Field, CreatorType
 from zotero.models import TaggedItem, FieldValue, CreatorValue
 
+#Hidden models
+#class HiddenModelAdmin(admin.ModelAdmin):
+#    def get_model_perms(self, *args, **kwargs):
+#        perms = admin.ModelAdmin.get_model_perms(self, *args, **kwargs)
+#        perms['list_hide'] = True
+#        return perms
+
+
 #Metadata
-#class FieldInline(admin.TabularInline):
-#    model = Field
-#    extra = 0
+class FieldInline(admin.TabularInline):
+    model = ItemType.fields.through
+    extra = 0
 
-#class CreatorTypeInline(admin.TabularInline):
-#    model = CreatorType
-#    extra = 0
 
-#class ItemTypeAdmin(admin.ModelAdmin):
-#    inlines = [FieldInline,CreatorTypeInline]
+class CreatorTypeInline(admin.TabularInline):
+    model = ItemType.creator_types.through
+    extra = 0
 
-#admin.site.register(ItemType,ItemTypeAdmin)
-admin.site.register(ItemType)
-admin.site.register(Field)
-admin.site.register(CreatorType)
+#class FieldAdmin(HiddenModelAdmin):
+#    inlines = [
+#        FieldInline,
+#    ]
+
+#class CreatorTypeAdmin(HiddenModelAdmin):
+#    inlines = [
+#        CreatorTypeInline,
+#    ]
+
+
+class FieldAdmin(admin.ModelAdmin):
+    inlines = [
+        FieldInline,
+    ]
+
+
+class CreatorTypeAdmin(admin.ModelAdmin):
+    inlines = [
+        CreatorTypeInline,
+    ]
+
+
+class ItemTypeAdmin(admin.ModelAdmin):
+    inlines = [
+        FieldInline,
+        CreatorTypeInline,
+    ]
+    exclude = (
+        'fields',
+        'creator_types',
+    )
+
+admin.site.register(ItemType, ItemTypeAdmin)
+admin.site.register(Field, FieldAdmin)
+admin.site.register(CreatorType, CreatorTypeAdmin)
+
 
 #Data
 class FieldValueInline(admin.TabularInline):
     model = FieldValue
     extra = 0
 
+
 class CreatorValueInline(admin.TabularInline):
     model = CreatorValue
     extra = 0
 
-class TaggedItemAdmin(admin.ModelAdmin):
-    inlines = [FieldValueInline,CreatorValueInline]
 
-admin.site.register(TaggedItem,TaggedItemAdmin)
+class TaggedItemAdmin(admin.ModelAdmin):
+    raw_id_fields = ("item_type",)
+    inlines = [
+        FieldValueInline,
+        CreatorValueInline,
+    ]
+
+admin.site.register(TaggedItem, TaggedItemAdmin)
--- zotero/models.py
+++ zotero/models.py
@@ -4,57 +4,65 @@ from django.contrib.contenttypes import generic
 
 
 # METADATA
-#Field in a document: title, publisher, publicationTitle, etc.
 class Field(models.Model):
+    '''Field in a document: title, publisher, publicationTitle, etc.'''
     field_name = models.CharField(max_length=100)
     
     def __unicode__(self):
         return self.field_name
 
-#Type of creator: author, editor, etc.
+
 class CreatorType(models.Model):
+    '''Type of creator: author, editor, etc.'''
     type_name = models.CharField(max_length=100)
     
     def __unicode__(self):
         return self.type_name
 
-#Type of document: book, journal article, etc.
+
 class ItemType(models.Model):
+    '''Type of document: book, journal article, etc.'''
     type_name = models.CharField(max_length=100)
     fields = models.ManyToManyField(Field)
-    creators_types = models.ManyToManyField(CreatorType)
+    creator_types = models.ManyToManyField(CreatorType)
     
     def __unicode__(self):
         return self.type_name
 
 
 #DATA
-#An instance of a tagged item: a book titled A, an article written by B, etc.
 class TaggedItem(models.Model):
+    '''An instance of a tagged item: a book titled "A", an article written by \
+    "B", etc.'''
     item_type = models.ForeignKey(ItemType)
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
-    content_object = generic.GenericForeignKey('content_type','object_id')
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
     
     def __unicode__(self):
-        fields_values = self.fieldvalue_set.all()
-        creators_values = self.creatorvalue_set.all()
-        return u'%s # %s # %s: (%s), (%s)' % (self.content_object,self.item_type,self.content_type,fields_values,creators_values)
+        field_values = self.fieldvalue_set.all()
+        creator_values = self.creatorvalue_set.all()
+        return u'item_type: %s, content_type: %s, object_id: %s, \
+        content_object: %s, fields: %s, creators: %s' % \
+        (self.item_type, self.content_type, self.object_id,
+        self.content_object, field_values, creator_values)
 
-#A value for a field of a tagged item: "title='Yerma'"
+
 class FieldValue(models.Model):
+    '''A value for a field of a tagged item: "title='Yerma'"'''
     field = models.ForeignKey(Field)
     value = models.CharField(max_length=256)
     tagged_item = models.ForeignKey(TaggedItem)
     
     def __unicode__(self):
-        return u'%s=%s' % (self.field,self.value)
+        return u'%s=%s' % (self.field, self.value)
 
-#A name for a creator of a tagged item: "author='Lorca'"
+
 class CreatorValue(models.Model):
+    '''A name for a creator of a tagged item: "author='Lorca'"'''
     creator_type = models.ForeignKey(CreatorType)
     value = models.CharField(max_length=256)
     tagged_item = models.ForeignKey(TaggedItem)
     
     def __unicode__(self):
-        return u'%s=%s' % (self.creator_type,self.value)
+        return u'%s=%s' % (self.creator_type, self.value)
