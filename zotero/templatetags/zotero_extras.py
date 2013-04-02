033fa5886b2c7f7a81921b0f57d1d8bf815fe4a4
Antonio Jimenez Mavillard <ajimene6@uwo.ca>
2013-04-02 09:54:25 -0400

    Added item type to meta tags. Modified object identifier on templatetag.

--- zotero/templatetags/zotero_extras.py
+++ zotero/templatetags/zotero_extras.py
@@ -11,21 +11,19 @@ def zotero_tags(parser, token):
     Returns the code to be translated by Zotero.
     Usage:
         {% zotero_tags
-            mod=object_model
-            oid=object_id
+            obj=app_label.model_name.object_id
             voc=vocabulary
             out=output_method %}
     Example:
-        {% zotero_tags mod=Document oid=1 voc=dc out=meta %}
+        {% zotero_tags obj=zotero.document.1 voc=dc out=meta %}
     """
     letters = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven']
     default = {
-        'mod': u'',
-        'oid': u'',
+        'obj': u'',
         'voc': u'dc',
         'out': u'meta'
     }
-    min_args = 2
+    min_args = 1
     max_args = len(default)
     if min_args == 1:
         min_pl = ''
@@ -58,9 +56,17 @@ def zotero_tags(parser, token):
             else:
                 default[key] = val
     
-    ct = ContentType.objects.get(app_label='zotero',
-                                 model=default['mod'].lower())
-    obj = ct.get_object_for_this_type(pk=default['oid'])
+    object_identifier = default['obj'].split(".")
+    if len(object_identifier) != 3:
+        raise TemplateSyntaxError('%s syntax error: object identifier.' % \
+                                  args[0])
+    app = object_identifier[0]
+    mod = object_identifier[1]
+    oid = object_identifier[2]
+    
+    ct = ContentType.objects.get(app_label=app,
+                                 model=mod)
+    obj = ct.get_object_for_this_type(pk=oid)
     
     return ZoteroTagsNode(ct, obj, default['voc'], default['out'])
 
@@ -85,11 +91,14 @@ def render_meta(model, obj, vocabulary):
     result = u'<link rel="schema.dc" href="http://purl.org/dc/elements/1.1/">'
     
     tags = Tag.objects.filter(content_type__pk=model.id, object_id=obj.id)
-    for tag in tags:
-        field = tag.field
-        name = field.namespaces.get(vocabulary, field.field_name)
-        value = tag.value
-        
-        tag = u'<meta property="DC.%s" content="%s"/>' % (name, value)
-        result = u'%s\n%s' % (result, tag)
+    if tags:
+        value = tags[0].item_type.type_name
+        meta_tag = u'<meta property="DC.type" content="%s"/>' % value
+        result = u'%s\n%s' % (result, meta_tag)
+        for tag in tags:
+            field = tag.field
+            name = field.namespaces.get(vocabulary, field.field_name)
+            value = tag.value
+            meta_tag = u'<meta property="DC.%s" content="%s"/>' % (name, value)
+            result = u'%s\n%s' % (result, meta_tag)
     return result
