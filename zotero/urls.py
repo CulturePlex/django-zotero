c32d3b85ea53ab4c4afa133a089437c824827985
Antonio Jimenez Mavillard <ajimene6@uwo.ca>
2013-05-07 14:29:50 -0400

    Formset for tags.

---
+++ zotero/urls.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import patterns, url
+
+from zotero import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<doc_id>\d+)/$', views.detail, name='detail'),
+)
