f821628ce53c158af3aebca31feeb518b9e85933
mavillard <ajimene6@uwo.ca>
2013-10-03 21:11:05 -0400

    Packaging

---
+++ setup.py
@@ -0,0 +1,50 @@
+import codecs
+import re
+from os import path
+from setuptools import setup, find_packages
+
+
+def read(*parts):
+    file_path = path.join(path.dirname(__file__), *parts)
+    return codecs.open(file_path).read()
+
+
+def find_version(*parts):
+    try:
+        version_file = read(*parts)
+        version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                                  version_file, re.M)
+        if version_match:
+            return version_match.group(1)
+        else:
+            return '1.0'
+    except:
+        return '1.0'
+#    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name='django-zotero',
+    version=find_version('django_zotero', '__init__.py'),
+    author='Antonio Jimenez Mavillard',
+    author_email='ajmavillard@gmail.com',
+    url='https://github.com/CulturePlex/django-zotero',
+    description='Django tool to tag objects and export them to Zotero',
+    long_description=read('README.rst'),
+    license='MIT',
+    keywords='zotero django admin tag meta metatag',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: JavaScript',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        ],
+    zip_safe=False,
+    packages=['zotero'],
+    include_package_data=True,
+    install_requires=['jsonfield'],
+)
